30e47d3078a0308a8d798d3f6f65326556ac0d18
AyaYamin <ayayamin4@gmail.com>
2019-04-11 18:26:20 +0300

    Add All Files

--- app/src/main/java/com/example/android/aya/parent_att.java
+++ app/src/main/java/com/example/android/aya/parent_att.java
@@ -1,23 +1,118 @@
 package com.example.android.aya;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class parent_att extends AppCompatActivity {
 
 
-    public void onBack(View view){
-        Intent intent1=new Intent(getApplicationContext(), ParentBody.class);
-        startActivity(intent1);
-        parent_att.this.finish();
-    }
-
+   ImageButton imageButton5;
+   TextView textView12;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_parent_att);
+
+        textView12=(TextView)findViewById(R.id.textView12);
+
+        Intent intent = getIntent();
+        final String id = intent.getStringExtra("id");
+        Log.i("id=",id);
+
+        imageButton5=(ImageButton)findViewById(R.id.imageButton5);
+        imageButton5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                Toast.makeText(parent_att.this, "Back is clicked!", Toast.LENGTH_SHORT).show();
+                Intent intent2=new Intent(getApplicationContext(),ParentBody.class);
+                intent2.putExtra("id", id);
+                startActivity(intent2);
+                parent_att.this.finish();
+            }
+
+        });
+
+        getJSON("http://192.168.1.6/Parent_Student/aya/getAbsence.php?id="+id);
     }
+
+
+    //this method is actually fetching the json string
+    private void getJSON(final String urlWebService) {
+        class GetJSON extends AsyncTask<Void, Void, String> {
+
+            @Override
+            protected void onPreExecute() {
+                super.onPreExecute();
+            }
+            @Override
+            protected void onPostExecute(String s) {
+                super.onPostExecute(s);
+                Toast.makeText(getApplicationContext(), s, Toast.LENGTH_SHORT).show();
+                textView12.setText("\n"+s);
+
+            }
+
+            //in this method we are fetching the json string
+            @Override
+            protected String doInBackground(Void... voids) {
+                try {
+                    //creating a URL
+                    URL url = new URL(urlWebService);
+
+                    //Opening the URL using HttpURLConnection
+                    HttpURLConnection con = (HttpURLConnection) url.openConnection();
+
+                    //StringBuilder object to read the string from the service
+                    StringBuilder sb = new StringBuilder();
+
+                    //We will use a buffered reader to read the string from service
+                    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(con.getInputStream()));
+
+                    //A simple string to read values from each line
+                    String json;
+
+                    //reading until we don't find null
+                    while ((json = bufferedReader.readLine()) != null) {
+
+                        //appending it to string builder
+                        sb.append(json + "\n");
+                    }
+
+                    Log.i("result of activity ", sb.toString());
+
+
+                    return sb.toString().trim();
+
+
+                } catch (Exception e) {
+                    return null;
+                }
+
+            }
+
+
+
+
+
+        }
+
+        //creating asynctask object and executing it
+        GetJSON getJSON = new GetJSON();
+        getJSON.execute();
+    }
+
+
 }
---
+++ getAbsence.php
@@ -0,0 +1,36 @@
+<?php
+
+  $db = new mysqli("localhost", "root", "", "project_new");
+  if (!$db) die("database connection error");
+  
+  $servername = "localhost";
+  $username = "root";
+  $password = "";
+  
+  
+  $conn = new mysqli($servername, $username, $password, "project_new");
+  
+  if ($conn->connect_error) {
+      die("Connection failed: " . $conn->connect_error);
+  }
+  
+	  $id=$_GET['id'];
+	
+      $sql = "SELECT absence FROM student WHERE part_id='$id'";
+      $result = mysqli_query($conn,$sql);
+      $count = mysqli_num_rows($result);
+      
+      if ($count > 0) {
+        while ($row = mysqli_fetch_assoc($result)) {
+          echo $row['absence'];
+        }
+    }
+
+
+mysqli_close($conn);
+?>
+
+
+
+
+
